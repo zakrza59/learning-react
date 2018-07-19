38b66705510aa418862a2f12ef18fb70ca7f1595
zakrza59 <zakrza59@gmail.com>
2018-07-19 22:16:55 +0200

    New features

--- src/App.js
+++ src/App.js
@@ -3,13 +3,50 @@ import Person from './Person/Person'
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    persons:[
+      {name: 'Max', age: 12},
+      {name: 'Zed', age: 15},
+      {name: 'Chris', age: 17}
+    ]
+  }
+
+  switchName = (newName) => {
+    this.setState({ 
+    persons:[
+      {name: newName, age: 2},
+      {name: 'Zeda', age: 5},
+      {name: 'Chrisa', age: 7}
+    ]})
+  }
+
+  nameChange = (event) => {
+    this.setState({ 
+    persons:[
+      {name: 'Chrisa', age: 2},
+      {name: event.target.value, age: 5},
+      {name: 'Chrisa', age: 7}
+    ]})
+  }
+  
+
   render() {
     return (
       <div className="App">
         <h1>Hi</h1>
-        <Person  name="Max" age="12"/>
-        <Person name="Zed" age="15">I like trains</Person>
-        <Person name="Chirs" age="17" />
+        <button onClick={() => this.switchName("Olav")} >Switch name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchName.bind(this, "Rupert")}
+          changed={this.nameChange}>and I like trains</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Person = (props) => {
   return (
-    <p>I'm {props.name} and I have {props.age}</p>
+    <div>
+      <p onClick={props.click}>I'm {props.name} and I have {props.age} {props.children}</p>
+      <input type="text" onChange={props.changed} />
+    </div>
   )
-  
-
 }
+
+export default Person;
