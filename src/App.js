f3030b334fa30c691b949c54303682f8efd24802
zakrza59 <zakrza59@gmail.com>
2018-07-19 21:27:04 +0200

    Made props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Person from './Person/Person'
 import './App.css';
 
 class App extends Component {
@@ -6,6 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi</h1>
+        <Person  name="Max" age="12"/>
+        <Person name="Zed" age="15"></Person>
+        <Person name="Chirs" age="17" />
       </div>
     );
   }
