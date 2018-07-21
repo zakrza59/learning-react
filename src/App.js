cac3034601914ee926f50300e27b04f1aec44f24
zakrza59 <zakrza59@gmail.com>
2018-07-22 01:22:46 +0200

    Added radium package

--- src/App.js
+++ src/App.js
@@ -56,7 +56,8 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: "white",
+      backgroundColor: "green",
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -78,11 +79,24 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = 'red';
     }
+
+    let classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); 
+    }
+
+    if (this.state.persons.length <= 1) {
+      classes.push('bold'); 
+    }
+
+
 
     return (
       <div className="App">
         <h1>Hi</h1>
+        <p className={classes.join(' ')}>This works</p>
         <button 
         style={style}
         onClick={this.togglePersonHandler} >Switch name</button>
