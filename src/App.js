c0de3b8fef7cd424e9d82b86ea5b197c500b42f7
zakrza59 <zakrza59@gmail.com>
2018-07-22 01:39:59 +0200

    added hover on btn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Person from './Person/Person'
+import Person from './Person/Person';
+import Radium from 'radium';
 import './App.css';
 
 class App extends Component {
@@ -61,7 +62,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -79,7 +84,12 @@ class App extends Component {
           })}
         </div>
       );
+      
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     let classes = [];
@@ -106,4 +116,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
