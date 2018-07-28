ebc0bb12edb0ae49c356690bee25537f914027e7
zakrza59 <zakrza59@gmail.com>
2018-07-28 22:42:23 +0200

    fixed dir

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import WithClasses from '../hoc/WithClass';
+import WithClasses from '../../hoc/WithClass';
 import classes from './Person.css';
 
 class Person extends Component {
   render() {
     return (
-      <WithClasses className={classes.Person}>
+      <WithClasses classes={classes.Person}>
         <p onClick={this.props.click}>I'm {this.props.name} and I have {this.props.age} {this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name} />
       </WithClasses>
